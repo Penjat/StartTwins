e66881b50e3d4171c97349e6aeaeb192798da03c
Penjat <theSpencerSymington@Gmail.com>
2019-03-15 16:39:48 -0700

    Initial Commit

---
+++ BlueStarRedStarTests/BlueStarRedStarTests.swift
@@ -0,0 +1,34 @@
+//
+//  BlueStarRedStarTests.swift
+//  BlueStarRedStarTests
+//
+//  Created by Spencer Symington on 2019-03-15.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+import XCTest
+@testable import BlueStarRedStar
+
+class BlueStarRedStarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
