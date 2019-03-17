0c9010e552c591cd88d08eeb84b5f8c75fa59a9c
Penjat <theSpencerSymington@Gmail.com>
2019-03-17 11:43:37 -0700

    tidy up

--- BlueStarRedStar/PieceFactory.swift
+++ BlueStarRedStar/PieceFactory.swift
@@ -4,14 +4,21 @@ import SpriteKit
 
 
 class PieceFactory{
+  var distNextSegment : CGFloat = 0.0
   
+  func checkShouldCreateSegment(movingNode:SKNode) -> Bool{
+    print("moving node = \(movingNode.position.y) , dist = \(distNextSegment)")
+     return distNextSegment > movingNode.position.y
   
+  }
   func createPiece(scene:GameScene){
     
     let piece = SKSpriteNode.init(texture: nil, color: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), size: CGSize(width: 40, height: 40))
-    piece.position = CGPoint(x: 0.0, y: 50.0)
+    let yPos = scene.frame.height/2 - scene.movingNode.position.y
+    piece.position = CGPoint(x: 0.0, y: yPos)
     scene.movingNode.addChild(piece)
     
-    
+    //TODO calculate properly with segment
+    distNextSegment = scene.movingNode.position.y - 200
   }
 }
