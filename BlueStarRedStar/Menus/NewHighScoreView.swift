9b00a78ab26e924fd5691aca5cccf7f61fc546e3
Penjat <theSpencerSymington@Gmail.com>
2019-03-19 20:10:15 -0700

    highscore menu appears when you get a highscore

---
+++ BlueStarRedStar/Menus/NewHighScoreView.swift
@@ -0,0 +1,27 @@
+
+
+import UIKit
+
+class NewHighScoreView: UIView {
+
+  @IBOutlet var contentView: UIView!
+  var delegate : MenuDelegate?
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonInit()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    commonInit()
+  }
+  
+  private func commonInit(){
+    Bundle.main.loadNibNamed("MenuNewHighScore", owner: self, options: nil)
+    addSubview(contentView)
+    contentView.frame = self.bounds
+    contentView.autoresizingMask = [.flexibleWidth , .flexibleHeight]
+  }
+
+}
