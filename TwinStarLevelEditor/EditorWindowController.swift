35dfbbc9a80612b082da7c629dbd9d05a39a47b3
Penjat <theSpencerSymington@Gmail.com>
2019-04-05 20:21:49 -0700

    can add menu items

--- TwinStarLevelEditor/EditorWindowController.swift
+++ TwinStarLevelEditor/EditorWindowController.swift
@@ -3,12 +3,17 @@
 import Cocoa
 
 class EditorWindowController: NSWindowController {
+  
 
-    override func windowDidLoad() {
-        super.windowDidLoad()
+  override func windowDidLoad() {
+    super.windowDidLoad()
+    let mainMenu = NSApplication.shared.mainMenu
+    let sectionsMenuItem = mainMenu?.item(at: 3)
+
+    let item = NSMenuItem(title: "Assmass", action: nil, keyEquivalent: "P")
+    sectionsMenuItem?.submenu?.addItem(item)
     
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-    }
+  }
   @IBAction func newLevel(_ sender: Any) {
     print("create new level")
   }
