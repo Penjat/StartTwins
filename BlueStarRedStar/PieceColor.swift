2d35d620197f8f6198306ae3f720c185f2616162
Penjat <theSpencerSymington@Gmail.com>
2019-04-17 13:18:09 -0700

    white and rand coins

--- BlueStarRedStar/PieceColor.swift
+++ BlueStarRedStar/PieceColor.swift
@@ -4,21 +4,7 @@ import Foundation
 
 
 enum PieceColor : Int{
-  case Gray = 0,Red = 1,Blue = 2,White = 3
+  case Gray = 0,Red = 1,Blue = 2,White = 3,Purple = 4
   
-  static func getPieceColor(num:Int)-> PieceColor{
-    
-    switch num{
-    case 0:
-      return Gray
-    case 1:
-      return Red
-    case 2:
-      return Blue
-    case 3:
-      return White
-    default:
-      return Gray
-    }
-  }
+  
 }
--- TwinStarLevelEditor/PieceColor+GetNSColor.swift
+++ TwinStarLevelEditor/PieceColor+GetNSColor.swift
@@ -14,7 +14,26 @@ extension PieceColor{
       return #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
     case .White:
       return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    case .Purple:
+      return #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    }
+  }
+  static func getPieceColor(num:Int)-> PieceColor{
+    
+    switch num{
+    case 0:
+      return .Gray
+    case 1:
+      return .Red
+    case 2:
+      return .Blue
+    case 3:
+      return .White
+    case 4:
+      return .Purple
       
+    default:
+      return Gray
     }
   }
 }
