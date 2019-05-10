f276ca875d2bc0e0eba7d11d6c14c6a898f56528
Penjat <theSpencerSymington@Gmail.com>
2019-05-10 10:47:21 -0700

    animate in buttons

--- BlueStarRedStar/Menus/MenuGameOverView.swift
+++ BlueStarRedStar/Menus/MenuGameOverView.swift
@@ -7,6 +7,9 @@ class MenuGameOverView: UIView , Menu{
   
   var delegate : MenuDelegate?
   
+  @IBOutlet weak var restartButton: UIButton!
+  @IBOutlet weak var titleButton: UIButton!
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     commonInit()
@@ -22,6 +25,27 @@ class MenuGameOverView: UIView , Menu{
     addSubview(contentView)
     contentView.frame = self.bounds
     contentView.autoresizingMask = [.flexibleWidth , .flexibleHeight]
+    animateButtons()
+  }
+  func animateButtons(){
+    
+    restartButton.alpha = 0.0
+    restartButton.transform = CGAffineTransform(translationX: 100.0, y: 0.0)
+    
+    titleButton.alpha = 0.0
+    titleButton.transform = CGAffineTransform(translationX: 100.0, y: 0.0)
+    
+    
+    UIView.animateKeyframes(withDuration: 4.0, delay: 4.0, options: [], animations: {
+      UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.5) {
+        self.restartButton.alpha = 1.0
+        self.restartButton.transform = CGAffineTransform(translationX: 0.0, y: 0.0)
+      }
+      UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+        self.titleButton.alpha = 1.0
+        self.titleButton.transform = CGAffineTransform(translationX: 0.0, y: 0.0)
+      }
+    }, completion: {_ in})
   }
 
   @IBAction func restartPressed(_ sender: Any) {
