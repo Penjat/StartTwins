846a4d015ed0b74949a50bf8f8094428b97869e8
Penjat <theSpencerSymington@Gmail.com>
2019-05-10 10:54:54 -0700

    fixed restart

--- BlueStarRedStar/GameScene/GameScene+MenuDelegate.swift
+++ BlueStarRedStar/GameScene/GameScene+MenuDelegate.swift
@@ -52,15 +52,6 @@ extension GameScene : MenuDelegate{
   func restart() {
     print("should restart the game")
     movingNode.position = CGPoint(x:0,y:0)
-    let movingNodeMove = SKAction.repeatForever(SKAction.moveBy(x: 0, y: -150.0, duration: 1))
-    movingNode.run(movingNodeMove)
-    isPlaying = true
-    pieceFactory.startGame(scene: self)
-    player.startGame(scene: self)
-    
-    //TODO function to animate out
-    //curMenu?.removeFromSuperview()
-    curMenu?.clear(menuCommand: .None)
-    
+    startGame()
   }
 }
