2312df609820e9cf957a44ae70ed1ea4db2e000d
Penjat <theSpencerSymington@Gmail.com>
2019-05-11 09:53:05 -0700

    added explosion for enemies

--- BlueStarRedStar/Pieces/Enemies/Hobbin+Enemy.swift
+++ BlueStarRedStar/Pieces/Enemies/Hobbin+Enemy.swift
@@ -18,6 +18,24 @@ extension Hobbin : Enemy{
     let sequence = SKAction.sequence([SKAction.group([fadeOut,growOut]),SKAction.removeFromParent() ])
     run(sequence)
     //removeFromParent()
+    
+    //Explode
+    let path = Bundle.main.path(forResource: "EnemyExplode", ofType: "sks")
+    var explosion = NSKeyedUnarchiver.unarchiveObject(withFile: path!) as! SKEmitterNode
+    
+    explosion.position = position
+    explosion.name = "rainParticle"
+    explosion.targetNode = self.scene
+    
+    //set to be piece color
+    explosion.particleColorSequence = nil;
+    explosion.particleColorBlendFactor = 1.0;
+    explosion.particleColor = pieceColor.getColor()
+    let staticNode = self.parent
+    staticNode?.addChild(explosion)
+  }
+  func explode(_ playerDelegate:PlayerDelegate){
+    
   }
   
   func getColor() -> PieceColor{
